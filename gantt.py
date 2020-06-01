0695f184ae839e191fcc115ca9ac420d3712a8b6
Felipe Foschiera <felipefoschiera@googlemail.com>
2020-06-01 13:58:23 -0300

    .

--- gantt.py
+++ gantt.py
@@ -5,7 +5,7 @@ matplotlib.style.use('ggplot')
 
 # Periodo 27/03 - 31/05
 start = datetime.datetime.strptime("27-03", "%d-%m")
-end = datetime.datetime.strptime("31-05", "%d-%m")
+end = datetime.datetime.strptime("02-06", "%d-%m")
 date_range = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
 
 x_axis = list(range(1, len(date_range)+1))
@@ -21,8 +21,8 @@ for date in date_range:
 # Formato dos dados: ('Nome da Série', [ ('dataBarra', duracao)], 'Texto inicial', 'Texto final')
 tuplas = [
     ('Space Force', [
-        ("30-05", 1)
-    ], '', 'S01E01-S01E05'),
+        ("30-05", 3)
+    ], 'S01E01-S01E10', ''),
     ('The Crown', [
         ("26-05", 1)
     ], '', 'S01E01'),
@@ -30,8 +30,8 @@ tuplas = [
         ("23-05", 2), ("29-05", 1)
     ], 'S01E01', 'S01E08'),
     ('Westworld', [
-        ("17-05", 1), ("21-05", 1), ("23-05", 1), ("25-05", 1), ("27-05", 2)
-    ], 'S01E01', 'S01E08'),
+        ("17-05", 1), ("21-05", 1), ("23-05", 1), ("25-05", 1), ("27-05", 2), ("01-06", 1)
+    ], 'S01E01', 'S01E09'),
     ('Fleabag', [
         ("14-05", 6)
     ], 'S01E01', 'S02E06'),
@@ -80,7 +80,7 @@ colors = [cmap(i/(len(tuplas)+1)) for i in range(len(tuplas))]
 # Configuração da figura
 fig, gnt = plt.subplots()
 
-plt.title("Séries assistidas durante a quarentena (27/03 - 30/05)")
+plt.title("Séries assistidas durante a quarentena (27/03 - 01/06)")
 gnt.grid(False)
 
 gnt.set_ylim(-1, len(tuplas))
